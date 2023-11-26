9490aeb5e29e3eee188d5c14f742cd629498d2cc
terry <terry@cleverbits.io>
2023-11-26 09:29:28 +0000

    how to manage an external dependency (service) without dependency injection and spring framework

--- src/main/java/guru/springframework/spring6di/controllers/MyController.java
+++ src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -1,12 +1,28 @@
 package guru.springframework.spring6di.controllers;
 
+import guru.springframework.spring6di.services.GreetingService;
+import guru.springframework.spring6di.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController() {
+        // set service manually to a concrete implementation - what we are doing is very rigid.
+        // We are creating a dependency here on the greeting service, but internal to the controller.
+        // So the controller has all the control as to how that greeting service is created and how it is
+        // managed, so right now this controller does not play nice with spring framework.
+        // This is not the way we want to be doing things - but I wanted to show you the concept here, where we
+        // are taking an external dependency that is going to be giving us a greeting, and this is one way you might
+        // do it without the Spring Framework and without using dependency injection.
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHello(){
         System.out.println("I'm in the Controller");
-        return "Hello Everyone from the Controller";
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/guru/springframework/spring6di/services/GreetingService.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring6di.services;
+
+// no annotations added for di
+public interface GreetingService {
+
+    String sayGreeting();
+}
